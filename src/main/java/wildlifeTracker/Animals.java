76e58760c333b0da72adcfb61128bee2720ca676
Antony Nganga <antonynganga05@gmail.com>
2019-04-29 23:17:25 +0300

    Add class endangered

--- src/main/java/wildlifeTracker/Animals.java
+++ src/main/java/wildlifeTracker/Animals.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 
 public class Animals {
-    private int id;
-    private String name;
+    public int id;
+    public String name;
+    public String type;
 
-    public Animals(String name) {
+    public Animals(String name, String type) {
         this.name = name;
+        this.type = type;
     }
 
     public int getId() {
@@ -19,6 +21,9 @@ public class Animals {
     public String getName() {
         return name;
     }
+    public String getType(){
+        return type;
+    }
     @Override
     public boolean equals(Object otherAnimal){
         if(!(otherAnimal instanceof Animals)){
@@ -26,14 +31,19 @@ public class Animals {
         }
         else{
             Animals newAnimal = (Animals) otherAnimal;
-            return this.getName().equals(newAnimal.getName());
+            return this.getName().equals(newAnimal.getName()) &&
+                    this.getType().equals(newAnimal.getType());
         }
     }
     public void save(){
+        if (name.equals("")) {
+            throw new IllegalArgumentException("Please enter a name.");
+        }
         try(Connection connect = DB.sql2o.open()){
-            String sql = "INSERT INTO animals (name) VALUES (:name);";
+            String sql = "INSERT INTO animals (name, type) VALUES (:name, :type);";
             this.id = (int) connect.createQuery(sql, true)
                     .addParameter("name", this.name)
+                    .addParameter("type", this.type)
                     .executeUpdate()
                     .getKey();
         }
@@ -42,6 +52,15 @@ public class Animals {
         String sql = "SELECT * FROM animals;";
         try(Connection connect= DB.sql2o.open()){
             return connect.createQuery(sql).executeAndFetch(Animals.class);
+        }
+    }
+    public static Animals find(int id){
+        try(Connection connect = DB.sql2o.open()){
+            String sql = "SELECT * FROM animals WHERE id = :id;";
+            Animals animal = connect.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Animals.class);
+                    return animal;
         }
     }
 }
--- src/test/java/wildlifeTracker/AnimalsTest.java
+++ src/test/java/wildlifeTracker/AnimalsTest.java
@@ -9,33 +9,46 @@ public class AnimalsTest {
 
     @Test
     public void animals_instantiatesCorrectly_true() {
-        Animals testAnimal = new Animals("Lion");
+        Animals testAnimal = new Animals("Lion", "yes");
         assertEquals(true, testAnimal instanceof Animals);
     }
     @Test
     public void Animals_instantiatesWithName_String() {
-        Animals testAnimal = new Animals("Lion");
+        Animals testAnimal = new Animals("Lion", "yes");
         assertEquals("Lion", testAnimal.getName());
     }
     @Test
+    public void Animals_instantiatesWithType_true(){
+        Animals testAnimal = new Animals("Lion", "yes");
+        assertEquals("yes", testAnimal.getType());
+    }
+    @Test
     public void equals_returnsTrueIfNamesAreEqual_true(){
-        Animals firstAnimal = new Animals("Lion");
-        Animals secondAnimal = new Animals("Lion");
+        Animals firstAnimal = new Animals("Lion", "yes");
+        Animals secondAnimal = new Animals("Lion", "yes");
         assertTrue(firstAnimal.equals(secondAnimal));
     }
     @Test
     public void save_insertObjectsIntoDatabase_Animals(){
-        Animals testAnimal = new Animals("Lion");
+        Animals testAnimal = new Animals("Lion", "yes");
         testAnimal.save();
         assertTrue(Animals.all().get(0).equals(testAnimal));
     }
     @Test
     public void all_returnsAllInstancesOfAnimals_true(){
-        Animals firstAnimal = new Animals("Lion");
+        Animals firstAnimal = new Animals("Lion", "yes");
         firstAnimal.save();
-        Animals secondAnimal = new Animals("Snake");
+        Animals secondAnimal = new Animals("Snake", "yes");
         secondAnimal.save();
         assertEquals(true, Animals.all().get(0).equals(firstAnimal));
         assertEquals(true, Animals.all().get(1).equals(secondAnimal));
+    }
+    @Test
+    public void find_returnsObjectWithTheSpecifiedId_firstAnimal(){
+        Animals firstAnimal = new Animals("Lion", "yes");
+        firstAnimal.save();
+        Animals secondAnimal = new Animals("Snake", "yes");
+        secondAnimal.save();
+        assertEquals(Animals.find(firstAnimal.getId()), firstAnimal);
     }
 }
