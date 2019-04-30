27a27dfb01dfd4f9188bffcf346c63f4e81bb844
Antony Nganga <antonynganga05@gmail.com>
2019-04-30 16:31:10 +0300

    Create test file for NonEndangered

--- src/test/java/wildlifeTracker/NonEndangeredTest.java
+++ src/test/java/wildlifeTracker/NonEndangeredTest.java
@@ -1,4 +1,16 @@
 package wildlifeTracker;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.List;
 
 public class NonEndangeredTest {
+    @Rule
+    public DatabaseRules database = new DatabaseRules();
+    @Test
+    public void NonEndangered_instantiatesCorrectly_true(){
+        NonEndangered testanimal = new NonEndangered("bees");
+        assertEquals(testanimal instanceof NonEndangered, true);
+    }
+    
 }
