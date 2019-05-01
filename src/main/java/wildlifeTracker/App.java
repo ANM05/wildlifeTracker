567cf30a2cef0fde719df65e52f6129299d1164f
Antony Nganga <antonynganga05@gmail.com>
2019-05-01 17:51:46 +0300

    Add animals template

--- src/main/java/wildlifeTracker/App.java
+++ src/main/java/wildlifeTracker/App.java
@@ -59,6 +59,14 @@ public class App {
             response.redirect("/animals");
             return null;
         });
-        
+
+        get("/animals", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("nonendangered", NonEndangered.all());
+            model.put("endangered", Endangered.all());
+            model.put("template", "templates/animals.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
     }
 }
