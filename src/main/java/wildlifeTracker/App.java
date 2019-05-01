17dbd70b41921f930af359a7d87d9c89b0c776dd
Antony Nganga <antonynganga05@gmail.com>
2019-05-01 17:25:19 +0300

    Add post method for animal form

--- src/main/java/wildlifeTracker/App.java
+++ src/main/java/wildlifeTracker/App.java
@@ -31,5 +31,34 @@ public class App {
             model.put("template", "templates/index.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+        get("/animals/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("HEALTHY", Endangered.HEALTHY);
+            model.put("OKAY", Endangered.OKAY);
+            model.put("ILL", Endangered.ILL);
+            model.put("NEWBORN", Endangered.NEWBORN);
+            model.put("YOUNG", Endangered.YOUNG);
+            model.put("ADULT", Endangered.ADULT);
+            model.put("rangerName", request.session().attribute("rangerName"));
+            model.put("template", "templates/animal-form.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        post("/animals/new", (request, response) -> {
+            String name = request.queryParams("name");
+            boolean endangered = request.queryParamsValues("endangered") != null;
+            if (endangered) {
+                String health = request.queryParams("health");
+                String age = request.queryParams("age");
+                Endangered endangeredAnimal = new Endangered(name, age, health);
+                endangeredAnimal.save();
+            } else {
+                NonEndangered notEndangered = new NonEndangered(name);
+                notEndangered.save();
+            }
+            response.redirect("/animals");
+            return null;
+        });
+        
     }
 }
